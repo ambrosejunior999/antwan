870eff4ba1fb22d2fdbb391bd66137f926a26d82
Ambrose Junior <adepojuifeoluwa999@gmail.com>
2022-06-12 22:57:26 +0100

    full one

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict mode'
+
+function h31() {
+    document.getElementById('dropdown1').style.display = 'inherit'
+    document.getElementById('dropdown1').style.transition = '0.3s'
+  }
+  function h11() {
+      document.getElementById('dropdown1').style.display = 'none'
+  }
+  function h32() {
+    document.getElementById('dropdown2').style.display = 'inherit'
+    document.getElementById('dropdown2').style.transition = '0.3s'
+  }
+  function h12() {
+      document.getElementById('dropdown2').style.display = 'none'
+  }
+  function h33() {
+    document.getElementById('dropdown3').style.display = 'inherit'
+    document.getElementById('dropdown3').style.transition = '0.3s'
+  }
+  function h13() {
+      document.getElementById('dropdown3').style.display = 'none'
+  }
+  function h34() {
+    document.getElementById('dropdown4').style.display = 'inherit'
+    document.getElementById('dropdown4').style.transition = '0.3s'
+  }
+  function h14() {
+      document.getElementById('dropdown4').style.display = 'none'
+  }
