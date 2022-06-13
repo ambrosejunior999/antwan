2667e0b726091d383e333758a323a9849f9adab3
Ambrose Junior <adepojuifeoluwa999@gmail.com>
2022-06-13 11:01:51 +0100

    small change

--- index.js
+++ index.js
@@ -4,27 +4,35 @@ function h31() {
     document.getElementById('dropdown1').style.display = 'inherit'
     document.getElementById('dropdown1').style.transition = '0.3s'
   }
-  function h11() {
+  function h01() {
       document.getElementById('dropdown1').style.display = 'none'
   }
   function h32() {
     document.getElementById('dropdown2').style.display = 'inherit'
     document.getElementById('dropdown2').style.transition = '0.3s'
   }
-  function h12() {
+  function h02() {
       document.getElementById('dropdown2').style.display = 'none'
   }
   function h33() {
     document.getElementById('dropdown3').style.display = 'inherit'
     document.getElementById('dropdown3').style.transition = '0.3s'
   }
-  function h13() {
+  function h03() {
       document.getElementById('dropdown3').style.display = 'none'
   }
   function h34() {
     document.getElementById('dropdown4').style.display = 'inherit'
     document.getElementById('dropdown4').style.transition = '0.3s'
   }
-  function h14() {
+  function h04() {
       document.getElementById('dropdown4').style.display = 'none'
+  }
+  function care() {
+   care =  prompt('WOULD YOU LIKE TO GET IN TOUCH WITH OUR CUSTOMER SERVICE') 
+   if(care ='yes'){
+       alert('PLS WAIT AS SOMEONE WILL GET IN TOUCH WITH YOU SOON')
+   }else{
+
+   }
   }
